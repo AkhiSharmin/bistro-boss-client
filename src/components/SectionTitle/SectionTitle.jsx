2e73ec0af8e500ad3c0fde9e0a1def29d2ba171b
AkhiSharmin <akhisharmin100@gmail.com>
2023-05-25 12:21:23 +0600

    added sectiontitle

---
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,12 @@
+const SectionTitle = ({ heading, subHeading }) => {
+  return (
+    <div className="mx-auto md:w-4/12 my-14">
+      <p className="text-yellow-600 text-center mb-4">--- {subHeading} ---</p>
+      <p className="text-4xl text-center uppercase border-y-4 py-4 ">
+        {heading}
+      </p>
+    </div>
+  );
+};
+
+export default SectionTitle;
