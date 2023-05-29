83478fb677ed1faa62da07f7ba8edc2c6b7ab105
AkhiSharmin <akhisharmin100@gmail.com>
2023-05-29 12:34:22 +0600

    added PopularMenu component

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -2,7 +2,7 @@ const SectionTitle = ({ heading, subHeading }) => {
   return (
     <div className="mx-auto md:w-4/12 my-14">
       <p className="text-yellow-600 text-center mb-4">--- {subHeading} ---</p>
-      <p className="text-4xl text-center uppercase border-y-4 py-4 ">
+      <p className="text-3xl text-center uppercase border-y-4 py-4 ">
         {heading}
       </p>
     </div>
