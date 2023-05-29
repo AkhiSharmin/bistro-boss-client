979a4804461adb66cf106d6a867356676319d9e4
AkhiSharmin <akhisharmin100@gmail.com>
2023-05-29 16:49:12 +0600

    added chef

---
+++ src/Pages/ChefRecommends/ChefRecommends.jsx
@@ -0,0 +1,77 @@
+import SectionTitle from "../../components/SectionTitle/SectionTitle";
+import img1 from "../../../assets/home/slide1.jpg";
+
+const ChefRecommends = () => {
+  return (
+    <div>
+      <div>
+        <SectionTitle
+          heading="Should Try"
+          subHeading="Chef RECOMMENDS"
+        ></SectionTitle>
+      </div>
+
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 mb-14">
+        <div className="card w-96 shadow-md">
+          <figure className="px-10 pt-10">
+            <img
+              src={img1}
+              alt="Shoes"
+              className=" 
+          w-[424px] h-[200px]"
+            />
+          </figure>
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">Caesar Salad</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions">
+              <button className="btn bg-slate-200 border-b-4 text-black border-b-orange-600 shadow-2xl border-0">
+                ADD TO CART
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-96 shadow-md">
+          <figure className="px-10 pt-10">
+            <img
+              src={img1}
+              alt="Shoes"
+              className=" 
+          w-[424px] h-[200px]"
+            />
+          </figure>
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">Caesar Salad</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions">
+              <button className="btn bg-slate-200 border-b-4 text-black border-b-orange-600 shadow-2xl border-0">
+                ADD TO CART
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-96 shadow-md">
+          <figure className="px-10 pt-10">
+            <img
+              src={img1}
+              alt="Shoes"
+              className=" 
+          w-[424px] h-[200px]"
+            />
+          </figure>
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">Caesar Salad</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions">
+              <button className="btn bg-slate-200 border-b-4 text-black border-b-orange-600 shadow-2xl border-0">
+                ADD TO CART
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChefRecommends;
