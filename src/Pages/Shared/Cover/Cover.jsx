243d2ab230f55675dc4fc4a63009ea2f8a0d45c9
AkhiSharmin <akhisharmin100@gmail.com>
2023-05-30 15:34:14 +0600

    added react tab

--- src/Pages/Shared/Cover/Cover.jsx
+++ src/Pages/Shared/Cover/Cover.jsx
@@ -3,12 +3,12 @@ import { Parallax } from "react-parallax";
 const Cover = ({ img, title }) => {
   return (
     <Parallax
-      blur={{ min: -15, max: 15 }}
+      blur={{ min: -50, max: 50 }}
       bgImage={img}
       bgImageAlt="the menu"
       strength={-200}
     >
-      <div className="hero h-[700px]">
+      <div className="hero h-[400px] ">
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
